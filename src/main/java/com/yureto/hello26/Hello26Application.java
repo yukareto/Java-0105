ed1509a96655d7df2cdcf2d385377b0d5718a94e
yureto <yut465p@gamil.com>
2024-01-05 21:31:28 +0900

    セットアップ

---
+++ src/main/java/com/yureto/hello26/Hello26Application.java
@@ -0,0 +1,12 @@
+package com.yureto.hello26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hello26Application.class, args);
+	}
+}
---
+++ src/test/java/com/yureto/hello26/Hello26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yureto.hello26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
